9254bb8b9c0bf3a619f33ef6265f3c568fee0a28
Riley Spires <24rspires@olsd.us>
2024-04-09 11:46:46 -0400

    WIP callApi()

--- src/Videolist.jsx
+++ src/Videolist.jsx
@@ -7,17 +7,25 @@ const VideoList = (props) => {
 
     const [videos, setVideos] = useState([]);
 
-    const callApi = () => {
+    const callApi = async () => {
+        const data = await fetch(`https://thebluealliance.com/api/v3/team/frc${teamNumber}/event/${eventKey}/matches`, {
+            headers: {
+                "X-TBA-Auth-Key": apiKey,
+            },
+        });
 
+        const json = data.json();
+
+        console.log(json);
     }
 
-    setInterval(callApi, 1000)
+    setInterval(callApi, 1000);
 
     return (
         <>
 
         </>
-    )
+    );
 }
 
 
